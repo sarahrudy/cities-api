b2bc1d0326b9785bf942f4e803ff36d7442f25de
Sarah <sarahrudy@gmail.com>
2021-11-06 16:19:19 -0600

    Add environment back in

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-// const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 const express = require('express');
 const app = express();
