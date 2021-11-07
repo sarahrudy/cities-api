625604198de83a888b0d3acc3051594c7f7f2611
Sarah <sarahrudy@gmail.com>
2021-11-06 19:17:33 -0600

    Revert

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (request, response) => {
 
 app.get('/cities', async (request, response) => {
   try {
-    const cities = await database('cities').insert();
+    const cities = await database('cities').select();
     console.log(cities)
     response.status(200).json(cities);
   } catch(error) {
