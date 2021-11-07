f016c1bbab84fb293fee8277d50974ca391f8915
Sarah <sarahrudy@gmail.com>
2021-11-06 19:01:02 -0600

    Add dependencies

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
-// Update with your config settings.
-const parse = require('pg-connection-string').parse
-const pgconfig = parse(process.env.DATABASE_URL)
-pgconfig.ssl = { rejectUnauthorized: false }
+// const parse = require('pg-connection-string').parse
+// const pgconfig = parse(process.env.DATABASE_URL)
+// pgconfig.ssl = { rejectUnauthorized: false }
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/cities',
@@ -17,26 +15,9 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
   production: {
     client: 'pg',
-    connection: pgconfig,
-    // process.env.DATABASE_URL + '?ssl=true&ssl=no-verify',
+    connection: process.env.DATABASE_URL + '?ssl=true&ssl=no-verify',
     migrations: {
       directory: './db/migrations'
     },
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
+const configuration = require('./knexfile.js')[environment];
 const database = require('knex')(configuration);
 
 const express = require('express');
